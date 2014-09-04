e532af9bf031f795ec52b3a655a73a9ba02191c3
Chai Phonbopit <phonbopit@gmail.com>
2014-09-05 00:35:57 +0700

    add demo

--- app/src/main/java/com/devahoy/navigationdrawerdemo/MyActivity.java
+++ app/src/main/java/com/devahoy/navigationdrawerdemo/MyActivity.java
@@ -2,35 +2,29 @@ package com.devahoy.navigationdrawerdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MyActivity extends Activity {
+
+    private String[] mDrawerTitle = {"Drawer1", "Drawer2", "Drawer3"};
+    private DrawerLayout mDrawerLayout;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mListView = (ListView) findViewById(R.id.drawer);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mDrawerTitle);
+
+        mListView.setAdapter(adapter);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
