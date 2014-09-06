b2bb075fbbc0f73f00c150a925c3a80f8318de55
Chai Phonbopit <phonbopit@gmail.com>
2014-09-06 13:02:05 +0700

    add ic_drawer.png

--- app/src/main/java/com/devahoy/navigationdrawerdemo/MyActivity.java
+++ app/src/main/java/com/devahoy/navigationdrawerdemo/MyActivity.java
@@ -1,22 +1,43 @@
 package com.devahoy.navigationdrawerdemo;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
 public class MyActivity extends Activity {
 
-    private String[] mDrawerTitle = {"Drawer1", "Drawer2", "Drawer3"};
+    public static final String KEY_DRAWABLE_ID = "drawableId";
+
+    private String[] mDrawerTitle = {"Cover", "Guitar", "Bass", "Drum"};
     private DrawerLayout mDrawerLayout;
     private ListView mListView;
+
+    private int[] imageResourceIds = {
+        R.drawable.cover,
+        R.drawable.guitar,
+        R.drawable.bass,
+        R.drawable.drum
+    };
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mListView = (ListView) findViewById(R.id.drawer);
@@ -25,6 +46,66 @@ public class MyActivity extends Activity {
                 android.R.layout.simple_list_item_1, mDrawerTitle);
 
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectFragment(position);
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close ) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+        selectFragment(0);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    private void selectFragment(int position) {
+        Fragment fragment = new MyFragment();
+        Bundle args = new Bundle();
+        args.putInt(KEY_DRAWABLE_ID, imageResourceIds[position]);
+        fragment.setArguments(args);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+
+        mListView.setItemChecked(position, true);
+        setTitle(mDrawerTitle[position]);
+        mDrawerLayout.closeDrawer(mListView);
     }
 
 }
--- app/src/main/java/com/devahoy/navigationdrawerdemo/MyFragment.java
+++ app/src/main/java/com/devahoy/navigationdrawerdemo/MyFragment.java
@@ -1,11 +1,12 @@
 package com.devahoy.navigationdrawerdemo;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MyFragment extends Fragment {
 
@@ -13,6 +14,13 @@ public class MyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        int resourceId = getArguments().getInt(MyActivity.KEY_DRAWABLE_ID);
+        View view = inflater.inflate(R.layout.fragment_my, container, false);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.image);
+        imageView.setImageResource(resourceId);
+
+        return view;
     }
 }
